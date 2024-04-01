da4deb4440be266759522473d826f6a9df31c919
NStefan002 <n.stefan002@gmail.com>
2024-04-01 13:21:35 +0200

    feat(donut_spawn): implement donut spawner

---
+++ lua/donut/donut.lua
@@ -0,0 +1,157 @@
+---@class Donut
+---@field win_size integer
+---@field theta_spacing number
+---@field phi_spacing number
+---@field illumination table
+---@field R1 number
+---@field R2 number
+---@field K2 number
+---@field K1 number
+---@field ns_id integer
+---@field bufnr integer
+---@field timer uv_timer_t
+local Donut = {}
+Donut.__index = Donut
+
+---@param bufnr integer
+---@param size integer
+function Donut.new(bufnr, size)
+    local self = setmetatable({}, Donut)
+    self.ns_id = vim.api.nvim_create_namespace("donut")
+    self.bufnr = bufnr
+    self.timer = nil
+
+    self.win_size = size
+    self.theta_spacing = 0.07
+    self.phi_spacing = 0.02
+    self.illumination = {
+        ".",
+        ",",
+        "-",
+        "~",
+        ":",
+        ";",
+        "=",
+        "!",
+        "*",
+        "#",
+        "$",
+        "@",
+    }
+    self.R1 = 1
+    self.R2 = 2
+    self.K2 = 5
+    self.K1 = self.win_size * self.K2 * 3 / (8 * (self.R1 + self.R2))
+    return self
+end
+
+---@param A number
+---@param B number
+---@return table
+function Donut:render_frame(A, B)
+    local cos_A, sin_A = math.cos(A), math.sin(A)
+    local cos_B, sin_B = math.cos(B), math.sin(B)
+
+    local output = {}
+    for _ = 1, self.win_size do
+        local tmp = {}
+        for _ = 1, self.win_size do
+            table.insert(tmp, " ")
+        end
+        table.insert(output, tmp)
+    end
+
+    local zbuffer = {}
+    for _ = 1, self.win_size do
+        local tmp = {}
+        for _ = 1, self.win_size do
+            table.insert(tmp, 0)
+        end
+        table.insert(zbuffer, tmp)
+    end
+
+    local theta = 0.0
+    while theta < 2 * math.pi do
+        local cos_theta, sin_theta = math.cos(theta), math.sin(theta)
+
+        local phi = 0.0
+        while phi < 2 * math.pi do
+            local cos_phi, sin_phi = math.cos(phi), math.sin(phi)
+
+            local circle_x = self.R2 + self.R1 * cos_theta
+            local circle_y = self.R1 * sin_theta
+
+            local x = circle_x * (cos_B * cos_phi + sin_A * sin_B * sin_phi)
+                - circle_y * cos_A * sin_B
+            local y = circle_x * (sin_B * cos_phi - sin_A * cos_B * sin_phi)
+                + circle_y * cos_A * cos_B
+            local z = self.K2 + cos_A * circle_x * sin_phi + circle_y * sin_A
+            local ooz = 1 / z
+
+            local xp = math.floor(self.win_size / 2 + self.K1 * ooz * x) + 1
+            local yp = math.floor(self.win_size / 2 - self.K1 * ooz * y) + 1
+
+            local L1 = cos_phi * cos_theta * sin_B
+                - cos_A * cos_theta * sin_phi
+                - sin_A * sin_theta
+                + cos_B * (cos_A * sin_theta - cos_theta * sin_A * sin_phi)
+
+            if L1 > 0 then
+                if zbuffer[xp] == nil or zbuffer[xp][yp] == nil then
+                    print(xp, yp)
+                end
+                if ooz > zbuffer[xp][yp] then
+                    zbuffer[xp][yp] = ooz
+                    local luminance_index = math.floor(L1 * 8) + 1
+                    output[xp][yp] = self.illumination[luminance_index]
+                end
+            end
+
+            phi = phi + self.phi_spacing
+        end
+        theta = theta + self.theta_spacing
+    end
+
+    return output
+end
+
+function Donut:display_frame(frame)
+    local output = {}
+    for i = 1, #frame do
+        output[i] = " "
+        for j = 1, #frame[i] do
+            output[i] = string.format("%s%s ", output[i], frame[i][j])
+        end
+    end
+    vim.api.nvim_buf_set_lines(self.bufnr, 0, #output, false, output)
+end
+
+function Donut:run()
+    local A = 1.0
+    local B = 1.0
+    self.timer = (vim.uv or vim.loop).new_timer()
+    self.timer:start(
+        0,
+        50,
+        vim.schedule_wrap(function()
+            A = A + self.theta_spacing
+            B = B + self.phi_spacing
+            self:display_frame(self:render_frame(A, B))
+        end)
+    )
+end
+
+function Donut:stop()
+    if self.timer then
+        self.timer:stop()
+        self.timer:close()
+        self.timer = nil
+    end
+    if self.bufnr ~= nil and vim.api.nvim_buf_is_valid(self.bufnr) then
+        vim.schedule(function()
+            vim.api.nvim_buf_delete(self.bufnr, { force = true })
+        end)
+    end
+end
+
+return Donut
--- lua/donut/init.lua
+++ lua/donut/init.lua
@@ -1,198 +1,74 @@
----@class Donut
----@field win_size integer
----@field theta_spacing number
----@field phi_spacing number
----@field illumination table
----@field R1 number
----@field R2 number
----@field K2 number
----@field K1 number
+-- TODO: add autocommands for window close, cursor move
+-- TODO: add highlights
+
+local Donut = require("donut.donut")
+
+---@class DonutSpawn
+---@field win_bufs table<integer, integer> buffers to restore after killing donuts
+---@field bufnrs integer[]
+---@field donuts Donut[]
+---@field win_wrap_options table<integer, boolean>
 ---@field ns_id integer
----@field bufnr integer
----@field winnr integer
-local Donut = {}
-Donut.__index = Donut
+local DonutSpawn = {}
+DonutSpawn.__index = DonutSpawn
 
----@param size integer
-function Donut.new(size)
-    local self = setmetatable({}, Donut)
+function DonutSpawn.new()
+    local self = setmetatable({}, DonutSpawn)
+    self.win_bufs = {}
+    self.bufnrs = {}
+    self.donuts = {}
+    self.win_wrap_options = {}
     self.ns_id = vim.api.nvim_create_namespace("donut")
-    self.bufnr = nil
-    self.winnr = nil
-
-    self.win_size = size
-    self.theta_spacing = 0.07
-    self.phi_spacing = 0.02
-    self.illumination = {
-        ".",
-        ",",
-        "-",
-        "~",
-        ":",
-        ";",
-        "=",
-        "!",
-        "*",
-        "#",
-        "$",
-        "@",
-    }
-    self.R1 = 1
-    self.R2 = 2
-    self.K2 = 5
-    self.K1 = self.win_size * self.K2 * 3 / (8 * (self.R1 + self.R2))
     return self
 end
 
----@param A number
----@param B number
----@return table
-function Donut:render_frame(A, B)
-    local cos_A, sin_A = math.cos(A), math.sin(A)
-    local cos_B, sin_B = math.cos(B), math.sin(B)
-
-    local output = {}
-    for _ = 1, self.win_size do
-        local tmp = {}
-        for _ = 1, self.win_size do
-            table.insert(tmp, " ")
-        end
-        table.insert(output, tmp)
-    end
-
-    local zbuffer = {}
-    for _ = 1, self.win_size do
-        local tmp = {}
-        for _ = 1, self.win_size do
-            table.insert(tmp, 0)
-        end
-        table.insert(zbuffer, tmp)
-    end
-
-    local theta = 0.0
-    while theta < 2 * math.pi do
-        local cos_theta, sin_theta = math.cos(theta), math.sin(theta)
-
-        local phi = 0.0
-        while phi < 2 * math.pi do
-            local cos_phi, sin_phi = math.cos(phi), math.sin(phi)
-
-            local circle_x = self.R2 + self.R1 * cos_theta
-            local circle_y = self.R1 * sin_theta
-
-            local x = circle_x * (cos_B * cos_phi + sin_A * sin_B * sin_phi)
-                - circle_y * cos_A * sin_B
-            local y = circle_x * (sin_B * cos_phi - sin_A * cos_B * sin_phi)
-                + circle_y * cos_A * cos_B
-            local z = self.K2 + cos_A * circle_x * sin_phi + circle_y * sin_A
-            local ooz = 1 / z
-
-            local xp = math.floor(self.win_size / 2 + self.K1 * ooz * x) + 1
-            local yp = math.floor(self.win_size / 2 - self.K1 * ooz * y) + 1
-
-            local L1 = cos_phi * cos_theta * sin_B
-                - cos_A * cos_theta * sin_phi
-                - sin_A * sin_theta
-                + cos_B * (cos_A * sin_theta - cos_theta * sin_A * sin_phi)
-
-            if L1 > 0 then
-                if zbuffer[xp] == nil or zbuffer[xp][yp] == nil then
-                    print(xp, yp)
-                end
-                if ooz > zbuffer[xp][yp] then
-                    zbuffer[xp][yp] = ooz
-                    local luminance_index = math.floor(L1 * 8) + 1
-                    output[xp][yp] = self.illumination[luminance_index]
-                end
-            end
-
-            phi = phi + self.phi_spacing
-        end
-        theta = theta + self.theta_spacing
-    end
-
-    return output
-end
-
-function Donut:display_frame(frame)
-    local output = {}
-    for i = 1, #frame do
-        output[i] = " "
-        for j = 1, #frame[i] do
-            output[i] = string.format("%s%s ", output[i], frame[i][j])
-        end
-    end
-    vim.api.nvim_buf_set_lines(self.bufnr, 0, #output, false, output)
-end
-
-function Donut:run()
-    self:create_window()
-    local A = 1.0
-    local B = 1.0
-    local i = self.win_size * self.win_size
-    local timer = (vim.uv or vim.loop).new_timer()
-    timer:start(
-        0,
-        50,
-        vim.schedule_wrap(function()
-            i = i - 1
-            if i == 0 then
-                timer:stop()
-                timer:close()
-                self:close_window()
-                return
-            end
-
-            A = A + self.theta_spacing
-            B = B + self.phi_spacing
-            self:display_frame(self:render_frame(A, B))
-        end)
-    )
-end
-
-function Donut:create_window()
-    local height = self.win_size
-    local width = self.win_size * 2
-
-    local cols = vim.o.columns
-    local lines = vim.o.lines - vim.o.cmdheight
+---@param winnr integer
+---@return integer bufnr
+local function put_buf_in_win(winnr)
     local bufnr = vim.api.nvim_create_buf(false, true)
-
-    local winnr = vim.api.nvim_open_win(bufnr, true, {
-        relative = "editor",
-        anchor = "NW",
-        row = math.floor((lines - height) / 2),
-        col = math.floor((cols - width) / 2),
-        width = width,
-        height = height,
-        style = "minimal",
-        border = "none",
-        noautocmd = true,
-    })
-
-    if winnr == 0 then
-        error("Donut: failed to open window")
-    end
-
-    self.bufnr = bufnr
-    self.winnr = winnr
-
-    vim.api.nvim_set_option_value("filetype", "donut", { buf = bufnr })
-    vim.wo[winnr].wrap = false
-    vim.api.nvim_win_set_hl_ns(self.winnr, self.ns_id)
-    -- self:create_autocmds()
+    vim.api.nvim_win_set_buf(winnr, bufnr)
+    return bufnr
 end
 
-function Donut:close_window()
-    if self.bufnr ~= nil and vim.api.nvim_buf_is_valid(self.bufnr) then
-        vim.api.nvim_buf_delete(self.bufnr, { force = true })
-    end
+function DonutSpawn:spawn_donuts()
+    local opened_windows = vim.api.nvim_list_wins()
+    for _, winnr in ipairs(opened_windows) do
+        -- save buffers to restore after killing donuts
+        self.win_bufs[winnr] = vim.api.nvim_win_get_buf(winnr)
 
-    if self.winnr ~= nil and vim.api.nvim_win_is_valid(self.winnr) then
-        vim.api.nvim_win_close(self.winnr, true)
+        local donut_bufnr = put_buf_in_win(winnr)
+        vim.api.nvim_set_option_value("filetype", "donut", { buf = donut_bufnr })
+        -- do not mess with the user's settings
+        self.win_wrap_options[winnr] = vim.wo[winnr].wrap
+        vim.wo[winnr].wrap = false
+        table.insert(self.bufnrs, donut_bufnr)
+
+        -- calculate donut size
+        local win_width = vim.api.nvim_win_get_width(winnr)
+        local win_height = vim.api.nvim_win_get_height(winnr)
+        local size = math.min(win_height, math.floor(win_width / 2))
+        local donut = Donut.new(donut_bufnr, size)
+        table.insert(self.donuts, donut)
+
+        donut:run()
     end
-    self.bufnr = nil
-    self.winnr = nil
 end
 
-return Donut
+function DonutSpawn:kill_donuts()
+    for win, buf in pairs(self.win_bufs) do
+        vim.schedule(function()
+            vim.api.nvim_win_set_buf(win, buf)
+        end)
+    end
+
+    for _, donut in ipairs(self.donuts) do
+        donut:stop()
+    end
+    self.bufnrs = {}
+
+    for winnr, wrap in pairs(self.win_wrap_options) do
+        vim.wo[winnr].wrap = wrap
+    end
+end
+
+return DonutSpawn
